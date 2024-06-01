d04d27204bec151c14bb42d10e62ce811bdeb2f0
gen04177 <159953537+gen04177@users.noreply.github.com>
2024-05-31 22:36:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+const int DISPLAY_TIME = 5000; // ms
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 SDL_Texture* gTexture = NULL;
-
-bool init();
-void close();
-bool loadMedia(const char* path);
-void render();
 
 bool init() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -43,7 +43,7 @@ bool init() {
     return true;
 }
 
-void close() {
+void closeSDL() {
     SDL_DestroyTexture(gTexture);
     SDL_DestroyRenderer(gRenderer);
     SDL_DestroyWindow(gWindow);
@@ -80,9 +80,47 @@ void render() {
     SDL_RenderPresent(gRenderer);
 }
 
+void closeTexture() {
+    SDL_DestroyTexture(gTexture);
+    gTexture = NULL;
+}
+
+void showImagesInDirectory(const char* directoryPath) {
+    DIR* directory = opendir(directoryPath);
+    if (directory == NULL) {
+        printf("Unable to open directory %s\n", directoryPath);
+        return;
+    }
+
+    struct dirent* entry;
+    while ((entry = readdir(directory)) != NULL) {
+
+        if (entry->d_type != DT_REG || entry->d_name[0] == '.') {
+            continue;
+        }
+
+        const char* fileName = entry->d_name;
+        const char* extension = strrchr(fileName, '.');
+        if (extension != NULL && (strcasecmp(extension, ".jpg") == 0 || strcasecmp(extension, ".bmp") == 0 || strcasecmp(extension, ".png") == 0)) {
+            char filePath[1024];
+            snprintf(filePath, sizeof(filePath), "%s/%s", directoryPath, fileName);
+            if (!loadMedia(filePath)) {
+                printf("Failed to load media: %s\n", filePath);
+                continue;
+            }
+
+            render();
+            SDL_Delay(DISPLAY_TIME);
+            closeTexture();
+        }
+    }
+
+    closedir(directory);
+}
+
 int main(int argc, char* args[]) {
     if (argc < 2) {
-        printf("Usage: %s <path_to_image>\n", args[0]);
+        printf("Usage: %s <path_to_image_or_directory>\n", args[0]);
         return 1;
     }
 
@@ -91,25 +129,44 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
-    if (!loadMedia(args[1])) {
-        printf("Failed to load media!\n");
+    struct stat path_stat;
+    if (stat(args[1], &path_stat) != 0) {
+        printf("Error accessing path %s\n", args[1]);
+        closeSDL();
         return 1;
     }
 
-    bool quit = false;
-    SDL_Event e;
+    if (S_ISDIR(path_stat.st_mode)) {
 
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
+        showImagesInDirectory(args[1]);
+
+    } else if (S_ISREG(path_stat.st_mode)) {
+
+        if (!loadMedia(args[1])) {
+            printf("Failed to load media!\n");
+            closeSDL();
+            return 1;
         }
 
-        render();
-        SDL_Delay(16);
+        bool quit = false;
+        SDL_Event e;
+
+        while (!quit) {
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
+
+            render();
+            SDL_Delay(16);
+        }
+    } else {
+        printf("%s is not a regular file or directory\n", args[1]);
+        closeSDL();
+        return 1;
     }
 
-    close();
+    closeSDL();
     return 0;
 }
